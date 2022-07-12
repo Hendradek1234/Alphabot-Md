b78116188d363c2ac43f01a3ecab3c25485f0a58
Hendradek1234 <108385707+Hendradek1234@users.noreply.github.com>
2022-07-13 01:12:00 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const chalk = require('chalk')
-const  { indonesia, english} = require(`./language`)
 
 // Website Api
 global.APIs = {
@@ -9,39 +8,32 @@ global.APIs = {
 
 // Free apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'V12',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz': 'your apikey',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
 global.autoread = false // auto read pesan / message
-global.autobio = true 
-
-//language
-//Available in indonesia & english
-//You can request others language 
-global.language = english  //change indonesia to english if you don't understand the language used by the bot
-
+//global.autorecording = true //status auto merekam ( auto record )
+//global.autoketik = false //status auto mengetik (auto typing)
+//global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐" //namabot kalian
+global.ownername= "𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐ ×፝֟͜×" //nama kalian
+global.myweb ="" //bebas asal jan hapus
+global.youtube = "" //bebas asal jan hapus
+global.github = "" //bebas
+global.email = "Hendradek@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "24105166540" // nomor wa kalian
+global.ownernomerr = "++24105166540" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["+24105166540","+24105166540","+24105166540"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© 𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐ོ' //sticker wm ubah
+global.author = 'Di Buat Oleh 𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐' //sticker wm ganti nama kalian
 global.sessionName = 'session'
-global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
